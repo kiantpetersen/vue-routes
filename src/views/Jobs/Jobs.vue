5e8559558d1a5da46f91517eb8757155ebe53f00
unknown <kiantpetersen@gmail.com>
2023-11-10 14:00:57 +0200

    adding jobs server api

--- src/views/Jobs/Jobs.vue
+++ src/views/Jobs/Jobs.vue
@@ -1,9 +1,14 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id">
-    <router-link :to="{name:'JobDetails',params:{id:job.id}}">
-      <h2>{{ job.title }}</h2>
-    </router-link>
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job.id">
+      <router-link :to="{name:'JobDetails',params:{id:job.id}}">
+        <h2>{{ job.title }}</h2>
+      </router-link>
+    </div>
+  </div>
+  <div v-else>
+    <h2>Loading Jobs...</h2>
   </div>
 
 </template>
@@ -12,13 +17,14 @@
 export default {
     data(){
         return {
-            jobs:[
-                {title:'UX Designer',id:1,details:'lorem'},
-                   { title:'Web Dev',id:2,details:'lorem'},
-                    {title:'Vue Dev',id:3,details:'lorem'},
-                
-            ]
+            jobs:[]
         }
+    },
+    mounted(){
+      fetch('http://localhost:3000/jobs')
+      .then((res)=>res.json())
+      .then((data)=>this.jobs=data)
+      .catch(err=>console.log(err.message))
     }
 
 }
